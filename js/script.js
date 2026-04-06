a5c240090172d8afbd4b1320100c31e9df8a797f
Shashank Tiwari <shashanktiwari1281@gmail.com>
2026-04-06 22:51:22 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide() {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+
+  index = (index + 1) % slides.length;
+}
+
+setInterval(showSlide, 3000);
+
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("active");
+}
